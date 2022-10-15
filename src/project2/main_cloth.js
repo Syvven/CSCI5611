d416fc9953dccc9510ad43007f6198f63a4b2e51
Noah Hendrickson <nhendrickson2001@gmail.com>
2022-10-15 11:40:11 -0500

    integrated gui and value changing

--- src/project2/main_cloth.js
+++ src/project2/main_cloth.js
@@ -1,11 +1,12 @@
 import * as THREE from '../node_modules/three/src/Three.js';
 import Stats from '../node_modules/stats.js/src/Stats.js';
+import GUI from '../node_modules/dat.gui/src/dat/gui/GUI.js';
 import {GLTFLoader} from './GLTFLoader.js';
 import {FlyControls} from './FlyControls.js';
 import {OrbitControls} from './OrbitControls.js';
 import {DragControls} from './DragControls.js';
 import WebGL from './webGLCheck.js';
-import { QuadraticBezierCurve } from '../node_modules/three/src/Three.js';
+
 
 // scene globals
 var scene, renderer, loader;
@@ -32,15 +33,22 @@ var restLen;
 var nodePos, nodeVel, nodeAcc, vertNodes, horizNodes;
 var currAcc, futureVel, futurePos;
 var objArr, controlArr;
+
+// stats and gui
 var totalDT, stats;
+var gui;
+var resetObj, pauseObj, dragObj;
 
 function setup() {
-    ///////////////////////// RENDERING INFO ////////////////////////////////////////////////////////
+    ///////////////////////// STATS AND GUI /////////////////////////////////////////////////////////
     totalDT = 0;
 
     stats = new Stats();
     stats.showPanel(0);
     document.body.appendChild(stats.dom);
+
+    gui = new GUI();
+    ///////////////////////// RENDERING INFO ////////////////////////////////////////////////////////
 
     scene = new THREE.Scene();
     scene.add(new THREE.AxesHelper(1000));
@@ -103,9 +111,9 @@ function setup() {
 
     floorY = 0.0; radius = 5.0;
     mass = 1; k = 100; kv = 50; kfric = 1;
-    dragC = 9; airD = 2;
+    dragC = 5; airD = 0.8;
     vertNodes = 15; horizNodes = 10;
-    gravity = new THREE.Vector3(0.0, -0.1, 0.0);
+    gravity = new THREE.Vector3(0.0, -1, 0.0);
     wind = new THREE.Vector3(0.0, 0.0, 0.4);
     stringTop = new THREE.Vector3(0.0, 50.0, 0.0);
     restLen = 3;
@@ -326,6 +334,39 @@ function setup() {
     }, undefined, function(error) {
         console.error(error);
     });
+
+    const windFolder = gui.addFolder('Wind Controls');
+    windFolder.add(wind, 'x', 0, 1, 0.01).name('Wind X');
+    windFolder.add(wind, 'y', 0, 1, 0.01).name('Wind Y');
+    windFolder.add(wind, 'z', 0, 1, 0.01).name('Wind Z');
+
+    dragObj = {
+        dragC: dragC,
+        airD: airD
+    }
+    const dragFolder = gui.addFolder('Drag Controls');
+    dragFolder.add(dragObj, 'dragC', 0, 15, 0.1).name('Drag Coefficient');
+    dragFolder.add(dragObj, 'airD', 0, 6, 0.1).name('Air Density');
+
+    const gravityFolder = gui.addFolder('Gravity Controls');
+    gravityFolder.add(gravity, 'y', -2, 0, 0.01).name('Gravity');
+
+    const simControlFolder = gui.addFolder('Sim Control');
+    resetObj = {
+        reset: false
+    };
+    const resetButton = simControlFolder.add(resetObj, 'reset');
+    resetButton.onChange(() => {
+        reset();
+    });
+
+    pauseObj = {
+        pause: true
+    };
+    const pauseButton = simControlFolder.add(pauseObj, 'pause');
+    pauseButton.onChange(() => {
+        paused = !paused;
+    });
 }
 
 function calculateMiscForces(pos, vels) {
@@ -431,16 +472,8 @@ function getRandomArbitrary(min, max) {
 
 var veltemp;
 function update(dt) {
-    if (totalDT % 1000 == 0) {
-        wind.x = getRandomArbitrary(-0.6, 0.6);
-        wind.z = getRandomArbitrary(-0.6, 0.6);
-    }
-    if (totalDT % 50 == 0) {
-        wind.x += getRandomArbitrary(-0.001, 0.001)
-    }
-    if (totalDT % 25 == 0 && totalDT % 50 != 0) {
-        wind.z += getRandomArbitrary(-0.001, 0.001)
-    }
+    dragC = dragObj.dragC;
+    airD = dragObj.airD;
     // start with current Velocity
     // calculate forces
     // currentAcceleration = function(current_velocity)
@@ -658,7 +691,6 @@ function animate() {
 // key handler booleans
 var paused = true;
 window.addEventListener( 'resize', onWindowResize, false );
-window.addEventListener('keyup', onKeyUp, false);
 window.addEventListener('touchstart', (e) => {
     e.preventDefault();
     orbitControls.enabled = false;
@@ -698,16 +730,6 @@ function reset() {
     topLeftNode.position.copy(ogTopLeftNodePos);
     topRightNode.position.copy(ogTopRightNodePos);
     updatePosAndColor();
-}
-
-function onKeyUp(event) {
-    if (event.code == 'Space') {
-        paused = !paused;
-    }
-
-    if (event.code == 'KeyR') {
-        reset();
-    }
 }
 
 setup();
